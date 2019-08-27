a8e7be846e3d30bec0ea0b46c7a6e1266c49e07e
MateMiklos <miklos.mate.91@gmail.com>
2019-08-27 17:14:10 +0200

    Inventory class has been added

---
+++ src/com/codecool/emem/game/entity/game_entity/Inventory.java
@@ -0,0 +1,26 @@
+package com.codecool.emem.game.entity.game_entity;
+
+import com.codecool.emem.game.gear.Armor;
+import com.codecool.emem.game.gear.Weapon;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Inventory {
+
+    private Weapon mainHandWeapon;
+    private Weapon offHandWeapon;
+    private Armor headArmor;
+    private Armor torsoArmor;
+    private Armor armsArmor;
+    private Armor legsArmor;
+
+    public List<Armor> getArmorSet() {
+        List<Armor> armorSet = new LinkedList<>();
+        armorSet.add(headArmor);
+        armorSet.add(torsoArmor);
+        armorSet.add(armsArmor);
+        armorSet.add(legsArmor);
+        return armorSet;
+    }
+}
